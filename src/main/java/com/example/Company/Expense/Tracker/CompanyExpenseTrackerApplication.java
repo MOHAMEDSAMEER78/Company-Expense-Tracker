b855b4625d406011ab283c39885db27166d96287
MOHAMEDSAMEER78 <mohamedsameermms@gmail.com>
2024-12-22 09:10:42 +0530

    initial commit

---
+++ src/main/java/com/example/Company/Expense/Tracker/CompanyExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Company.Expense.Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyExpenseTrackerApplication.class, args);
+	}
+
+}
